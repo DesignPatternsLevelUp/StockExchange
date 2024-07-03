d3c38c99160b79755ddb4295863504443bb648b2
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-07-03 20:28:34 +0200

    purge

--- database/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./transaction";
-export * from "./company";
-export * from "./person";
-export * from "./owner";
-export * from "./share";
--- services/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./transaction-service";
-export * from "./owner-service";
-export * from "./company-service";
-export * from "./share-service";
-export * from "./errors";
