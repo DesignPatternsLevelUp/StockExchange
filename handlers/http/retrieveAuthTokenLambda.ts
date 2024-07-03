5e0c22e0cfa1535d3af83151bf28f4ecee5423a6
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-07-03 22:58:11 +0200

    post

--- handlers/http/retrieveAuthTokenLambda.ts
+++ handlers/http/retrieveAuthTokenLambda.ts
@@ -8,7 +8,11 @@ export const handler: APIGatewayProxyHandler = async (event, context) => {
     console.log(`Event: ${JSON.stringify(event, null, 2)}`);
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     const code = parseInput<{ code: string }>(event)?.code;
-    const res = await fetch(`${process.env.tokenEndpoint}?client_id=${process.env.clientId}&client_secret=${process.env.clientSecret}&redirect_uri=${new URL(process.env.redirectUrl ?? '' )}&grant_type=authorization_code&code=${code}`);
+    const res = await fetch(`${process.env.tokenEndpoint}?client_id=${process.env.clientId}&client_secret=${process.env.clientSecret}&redirect_uri=${new URL(process.env.redirectUrl ?? '' )}&grant_type=authorization_code&code=${code}`,
+    {
+        method: "POST"
+    }
+    );
     console.log(`${process.env.tokenEndpoint}?client_id=${process.env.clientId}&client_secret=${process.env.clientSecret}&redirect_uri=${new URL(process.env.redirectUrl ?? '' )}&grant_type=authorization_code&code=${code}`);
     if (res.ok){
         let body = await res.json();
