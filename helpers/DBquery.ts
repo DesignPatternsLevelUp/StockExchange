da9c80b71eb64c6bd782a7ce4d3dcc12a742a7b6
Liam Quick <liam.quick@vcontractor.co.za>
2024-07-03 16:22:40 +0200

    Logging DB Config

--- helpers/DBquery.ts
+++ helpers/DBquery.ts
@@ -1,13 +1,15 @@
 import {Client, QueryResultRow} from "pg";
 
 export async function withClient<T>(handler: (client: Client) => T) {
-    const client = new Client({
+    const config = {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
         password: process.env.DB_PASSWORD,
         port: parseInt(process.env.DB_PORT || '5432'),
-    });
+    };
+    console.log('DB Config:', config);
+    const client = new Client(config);
 
     try {
         return handler(client);
